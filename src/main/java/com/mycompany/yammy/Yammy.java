539b3ff4b05753c234d2f25e3369d9ff05d8bc41
Asher Mathews <msasher123@gmail.com>
2022-12-29 21:27:05 +0530

    initial commit

---
+++ src/main/java/com/mycompany/yammy/Yammy.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.yammy;
+
+/**
+ *
+ * @author Asher
+ */
+public class Yammy {
+
+    public static void main(String[] args) {
+        System.out.println("Application Starting.........");
+        main_frame.display();
+    }
+}
